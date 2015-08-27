9895b844d844492970bacfcc65a1f4b213b1067f
Linus Ericsson <oscarlinusericsson@gmail.com>
2015-08-27 23:42:58 +0200

    add enlive dep

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject snipsnip "0.1.0-SNAPSHOT"
+  :description "Enlive with short feedback loop"
+  :url "http://github.com/claj/snipsnip"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [enlive "1.1.6"]])
